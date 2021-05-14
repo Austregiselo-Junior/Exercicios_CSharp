cb99f506e10e8ba4ec43cb9ca72f637ac83dcab6
Austregíselo Junior <austregiselosvj@hotmail.com>
2021-05-14 17:18:01 -0300

    Add files via upload

---
+++ Bibliotecas CLLs, documentação e usando o NuGet - 5/AluraStudio-CSharpPT5-b0bdb1e09d442e7dd25ca018d02d2ad5ce9c4405/ByteBank/ByteBank.Modelos/ContaCorrente.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    /// <summary>
+    /// Essa classe representa uma conta corrente do banco ByteBank
+    /// </summary>
+    public class ContaCorrente
+    {
+        private static int TaxaOperacao;
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+        public Cliente Titular { get; set; }
+
+        public int ContadorSaquesNaoPermitidos { get; private set; }
+        public int ContadorTransferenciasNaoPermitidas { get; private set; }
+
+        public int Numero { get; }
+        public int Agencia { get; }
+
+        private double _saldo = 100;
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        /// <summary>
+        /// Cria uma instancia da conta corrente com argumentos
+        /// </summary>
+        /// <param name="agencia">Representa o valor da propriedade <see cref="Agencia"/> e deve possuir um valor maior que zero.</param>
+        /// <param name="numero">Representa o valor da propriedade <see cref="Numero"/> e deve possuir um valor maior que zero.</param>
+        public ContaCorrente(int agencia, int numero)
+        {
+            if (numero <= 0)
+            {
+                throw new ArgumentException("O argumento agencia deve ser maior que 0.", nameof(agencia));
+            }
+
+            if (numero <= 0)
+            {
+                throw new ArgumentException("O argumento numero deve ser maior que 0.", nameof(numero));
+            }
+
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+        }
+
+        /// <summary>
+        /// Realiza o saque e atualiza o valor da propriedade Saldo.
+        /// </summary>
+        /// <param name="valor">Representa o valor do saque, deve ser maior que 0 e menor que o <see cref="Saldo"/>.</param>
+        /// <exception cref="ArgumentException">Exceção lançada quando um valor negativo é utilizado no argumento <paramref name="valor"/>.</exception>
+        /// <exception cref="SaldoInsuficienteException"> Exceção lançada quando o valor de <paramref name="valor"/> é maior que o valor da propriedade <see cref="Saldo"/>. </exception>
+        public void Sacar(double valor)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor inválido para o saque.", nameof(valor));
+            }
+
+            if (_saldo < valor)
+            {
+                ContadorSaquesNaoPermitidos++;
+                throw new SaldoInsuficienteException(Saldo, valor);
+            }
+
+            _saldo -= valor;
+        }
+
+        /// <summary>
+        /// Método de faz o saque do valor <paramref name="valor"/>
+        /// </summary>
+        /// <param name="valor"></param>
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        /// <summary>
+        /// Faz tranferencias do valor <paramref name="valor"/> para a conta <paramref name="contaDestino"/>
+        /// </summary>
+        /// <param name="valor">Deve ser maior que 0 e maior que <see cref="Saldo"/>.</param>
+        /// <param name="contaDestino"></param>
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor inválido para a transferência.", nameof(valor));
+            }
+
+            try
+            {
+                Sacar(valor);
+            }
+            catch (SaldoInsuficienteException ex)
+            {
+                ContadorTransferenciasNaoPermitidas++;
+                throw new OperacaoFinanceiraException("Operação não realizada.", ex);
+            }
+
+            contaDestino.Depositar(valor);
+        }
+    }
+
+}
