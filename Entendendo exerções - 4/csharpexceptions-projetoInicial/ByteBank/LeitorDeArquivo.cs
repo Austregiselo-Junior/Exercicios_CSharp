06eb6a31d1cdfb61e8cd11404b3040b968757280
Austregíselo Junior <austregiselosvj@hotmail.com>
2021-05-14 17:17:06 -0300

    Add files via upload

---
+++ Entendendo exerções - 4/csharpexceptions-projetoInicial/ByteBank/LeitorDeArquivo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ByteBank
+{
+    public class LeitorDeArquivo : IDisposable
+    {
+        public string Arquivo { get; set; }
+
+        public LeitorDeArquivo(string arquivo)
+        {
+            Arquivo = arquivo;
+
+             throw new FileNotFoundException("Arquivo não encontrado.");
+            Console.WriteLine("Abrindo arquivo: " + arquivo);
+        }
+
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha. . .");
+
+             throw new IOException("Arquivo não corrompido.");
+            return "Linha do arquivo";
+        }
+
+      
+        public void Dispose()
+        {
+              Console.WriteLine("Fechando arquivo.");
+        }
+    }
+}
