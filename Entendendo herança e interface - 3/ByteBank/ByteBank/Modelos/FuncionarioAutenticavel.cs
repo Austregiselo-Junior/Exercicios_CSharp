58c02b72d8e54519de276e430b7ea419ff6cb5de
Austregíselo Junior <austregiselosvj@hotmail.com>
2021-05-14 17:16:40 -0300

    Add files via upload

---
+++ Entendendo herança e interface - 3/ByteBank/ByteBank/Modelos/FuncionarioAutenticavel.cs
@@ -0,0 +1,24 @@
+﻿using ByteBank.Funcionarios;
+using ByteBank.SistemaInterno;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    public abstract class FuncionarioAutenticavel : Funcionariosf, IAutenticavel
+    {
+        public string Senha { get; set; }
+
+        public FuncionarioAutenticavel(double salario, string cpf) : base(cpf, salario)
+        {
+        }
+
+      public bool Autenticar(string senha)
+        {
+            return Senha == senha;
+        }
+    }
+}
