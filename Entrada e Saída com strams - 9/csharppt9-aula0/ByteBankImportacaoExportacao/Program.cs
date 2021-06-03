bd98834ab072e67f7ce89e2d415291a36618f9f4
Austregíselo Junior <austregiselosvj@hotmail.com>
2021-06-03 16:58:09 -0300

    Add files via upload

---
+++ Entrada e Saída com strams - 9/csharppt9-aula0/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,29 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arquivo = "contas.txt";
+            using (var fluxoDeArquivo = new FileStream(arquivo, FileMode.Open))
+            using (var leitor = new StreamReader(fluxoDeArquivo))
+            {
+                while (!leitor.EndOfStream)
+                {
+                    var linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
+                }
+                Console.ReadLine();
+            }
+        }
+               
+    }
+}
