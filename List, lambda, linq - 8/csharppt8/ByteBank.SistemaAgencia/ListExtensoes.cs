2d07ab3cb9b61faf4d4d277a9a1ca048de200511
Austregíselo Junior <austregiselosvj@hotmail.com>
2021-06-03 16:57:32 -0300

    Add files via upload

---
+++ List, lambda, linq - 8/csharppt8/ByteBank.SistemaAgencia/ListExtensoes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+
+
+   static class ListExtensoes
+    {
+        public static void AdicionarVarios<T>(this List<T> ListaDeInteiros, params T[] itens)
+        {
+            foreach (var item in itens)
+            {
+                ListaDeInteiros.Add(item);
+            }
+        }
+
+       
+    }
+}
