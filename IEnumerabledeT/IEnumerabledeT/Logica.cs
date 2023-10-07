0a09826c2f48142cc67fbb806ddb546b7fa51de8
Austregíselo Soares de Vasconcelos Junior <105051189+Austregiselo-Junior@users.noreply.github.com>
2023-10-07 16:07:12 -0300

    IEnumerabledeT
    IEnumerabledeT

---
+++ IEnumerabledeT/IEnumerabledeT/Logica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerabledeT
+{
+    internal class Logica
+    {
+      public IEnumerable<int> square(int max)
+        {
+            for (int i = 0; i < max; i++) { yield return i * i; } // com o Yeald é mais perfomático
+        }
+			
+    }
+}
---
+++ IEnumerabledeT/IEnumerabledeT/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerabledeT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var logica = new Logica();
+
+            //IEnumerable<T>
+            var squares = logica.square(5); // Essa linha não executa imediatamente o Square, porque ele tem op Yield, que faz com que seja aguardado o iterador
+
+            foreach (var square in squares) { Console.WriteLine($"square: {square}"); }
+
+            Console.ReadLine();
+
+           // IEnumerable<T> e LINQ
+           var numeros = new List<int> {1,5,2,8,9 };
+
+            var addnumeros = numeros.Where(n => n % 2 != 0).Select(num => num);
+
+            foreach (var item in addnumeros)
+            {
+                Console.WriteLine($"item: {item}");
+            }
+            Console.ReadLine();
+            // Neste exemplo, a oddNumbersvariável tem o tipo IEnumerable<int>.Devido à avaliação da consulta adiada, a consulta não é executada até a foreachinstrução.
+        }
+    }
+}
