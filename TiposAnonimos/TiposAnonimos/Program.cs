df4c3ffa80ff75a3c26dddc46376b55e69455d70
Austregíselo Soares de Vasconcelos Junior <105051189+Austregiselo-Junior@users.noreply.github.com>
2023-10-07 16:15:08 -0300

    TiposAnonimos
    TiposAnonimos

---
+++ TiposAnonimos/TiposAnonimos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposAnonimos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------  Criando um tipo anônimo -----------------------");
+            // O exemplo a baixo mostra a criação de um tipo anônimo atravé do "new".
+
+            var product = new { Name = "Phone X", Price = 99.00 }; // Observe que não há o tipo especificado como seria numa classe.
+            Console.WriteLine($"{product.Name}, {product.Price}");
+
+            Console.WriteLine("-------------------------  Criando tipos anônimos com tipos explicito -----------------------");
+            TiposAnonimosdeTipoExplicito();
+            Console.ReadLine();
+
+        }
+
+        private static void TiposAnonimosdeTipoExplicito()
+        {
+            var product = new
+            {
+                Name = "Phone X",
+                Price = 99.9,
+                atribute = new Atributos() { Color = "Black", Weight = 100 },
+            };
+
+            Console.WriteLine($"Name: {product?.Name}, Color: {product.atribute?.Color}");
+
+        }
+
+
+    }
+}
---
+++ TiposAnonimos/TiposAnonimos/TipoAnonimodeumTipoExplicito/Atributos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposAnonimos
+{
+    internal class Atributos
+    {
+        public string Color { get; set; }
+        public int Weight { get; set; }
+
+    }
+
+    
+}
